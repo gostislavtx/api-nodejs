4df090c716976f4394c24b4caba94e69051cf362
gostislavtx <luizbonatex@gmail.com>
2025-08-24 20:59:07 -0300

    Inicializando projeto Node.js com Express

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/usuarios', (req, res) => {
+    res.send('Ok, deu bom')
+})
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000')
+})
